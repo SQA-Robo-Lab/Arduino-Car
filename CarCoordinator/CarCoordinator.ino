a46bd686bf69395eeca597a038f028c7d5b165d4
Code-Schwabe <st170535@stud.uni-stuttgart.de>
2022-02-16 14:23:08 +0100

    Add examplary communication

--- CarCoordinator/CarCoordinator.ino
+++ CarCoordinator/CarCoordinator.ino
@@ -1,7 +1,38 @@
-void setup(){
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <WiFiEsp.h>
 
+const byte ownI2CAddress = 9;
+const byte driverI2CAddress = 8;
+
+char ssid[] = "Davids Galaxy A52s";
+char pass[] = "********";
+int status = WL_IDLE_STATUS;
+SoftwareSerial EspSerial(2,3);
+
+void setup(){
+    Serial.begin(9600);
+
+    Wire.begin(ownI2CAddress);
+
+    EspSerial.begin(9600);
+    WiFi.init(&EspSerial);
+
+    while(status != WL_CONNECTED){
+        status = WiFi.begin(ssid, pass);
+    }
 }
 
 void loop(){
-    
+    Wire.beginTransmission(driverI2CAddress);
+    Wire.write("START");
+    Wire.endTransmission();
+    Serial.println("START");
+    delay(random(2000, 7000));
+
+    Wire.beginTransmission(driverI2CAddress);
+    Wire.write("STOP");
+    Wire.endTransmission();
+    Serial.println("STOP");
+    delay(random(2000, 7000));
 }
--- LineFollower/LineFollower.ino
+++ LineFollower/LineFollower.ino
@@ -3,15 +3,26 @@
  * @author david
  * @brief Demonstration code for a line follower car. 
  */
+#include <Wire.h>
 
 #include "./MotorDriver.c"
 #include "./LineDetector.c"
 #include "./DistanceSensor.c"
 
 LinePosition position;
-int base_speed = 60;
+int baseSpeed = 60;
+
+const byte ownI2CAddress = 8;
+const byte coordinatoI2CAddress = 9;
+
+char i2cBuffer[32];
+boolean i2cNewMessageFlag = false;
+
+boolean drivingAllowed = true;
 
 void setup(){
+    Wire.begin(ownI2CAddress);
+    Wire.onReceive(receiveI2CMessage);
     Serial.begin(9600);
     initMotorDriver();
 
@@ -24,27 +35,50 @@ void setup(){
 }
 
 void loop(){
-    if (getFrontDistance() < 5){
+    if (i2cNewMessageFlag) {
+        // Serial.println(i2cBuffer);
+        i2cNewMessageFlag = false;
+        if (strncmp(i2cBuffer, "STOP", 4) == 0){
+            drivingAllowed = false;
+        } else {
+            drivingAllowed = true;
+        }
+        // Serial.println(drivingAllowed);
+    }
+    
+    int d = getFrontDistance();
+    // Serial.println(d);
+    if (d < 10){
+        stop();
+    } else if (!drivingAllowed){
         stop();
     } else {
-        followLine(base_speed);
+        followLine(baseSpeed);
     }
     // delay(100);
+    // followLine(baseSpeed);
 }
 
 void followLine(int speed){
     position = detectPosition(position);
     if (position == ON_LINE){
-        Serial.println("On line!");
+        // Serial.println("On line!");
         driveForward(speed);
     } else if (position == LEFT_OF_LINE){
-        Serial.println("LEFT of line!");
+        // Serial.println("LEFT of line!");
         turnRightForward(speed*1.5);
     } else if (position == RIGHT_OF_LINE){
-        Serial.println("RIGHT of line");
+        // Serial.println("RIGHT of line");
         turnLeftForward(speed*1.5);
     } else {
         Serial.println("This is strange...");
         stop();
     }
+}
+
+void receiveI2CMessage(int numberOfBytesReceived){
+    // Serial.println("Message received!");
+    memset(i2cBuffer, 0, sizeof(i2cBuffer));
+    Wire.readBytes(i2cBuffer, numberOfBytesReceived);
+    i2cNewMessageFlag = true;
 }
