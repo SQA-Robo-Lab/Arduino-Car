7a235aa800f86d671aa356990781623988cf4f6b
ModellbahnFreak <freak.mdellbahn@gmail.com>
2023-06-15 13:39:35 +0200

    Extracted new motor driver to https://github.com/SQA-Robo-Lab/Sofdcar-HAL

--- NewMotorDriver/motorDebug.cpp
+++
@@ -1,9 +0,0 @@
-#include "Arduino.h"
-#include "src/driveController/DriveController.hpp"
-
-uint8_t speed, speedBuffer, motorNum = 255, directionBuffer, i2cSendBufferIndex = 255;
-char i2cSendBuffer[32];
-
-void motorDebugLoop(DriveController &dc)
-{
-}
--- NewMotorDriver/src/motors/SteerableAxle.cpp
+++
@@ -1,6 +0,0 @@
-#include "SteerableAxle.hpp"
-
-SteerableAxle::SteerableAxle(uint8_t maxSteeringLeftAngle, uint8_t maxSteeringRightAngle)
-    : maxLeftAngle(maxSteeringLeftAngle), maxRightAngle(maxSteeringRightAngle)
-{
-}
